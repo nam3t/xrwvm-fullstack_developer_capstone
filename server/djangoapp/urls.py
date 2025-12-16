e4ad435d87ea05a89a3e0c41e959d71663560a5a
nam3t <namnh2711@gmail.com>
2025-12-16 15:40:19 +0700

    Refactor import statement in urls.py
    - Uncommented the import statement for views to ensure proper routing functionality in the Django application.

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
