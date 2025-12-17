a17c90c123a5840cf196d28ff86c75a0670c890d
nam3t <namnh2711@gmail.com>
2025-12-17 23:34:55 +0700

    add import

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,7 +14,7 @@ import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .models import CarMake, CarModel
-# from .populate import initiate
+from .populate import initiate
 
 
 # Get an instance of a logger
